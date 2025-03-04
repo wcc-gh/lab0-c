1c16fecdbcf73b483ae0ecd56419a6a30080d1c3
Wei-Cheng Chen <chen1048.tw@gmail.com>
2025-03-05 00:06:38 +0800

    Fix q_free
    Corrected q_free to use the element_t structure.
    
    Change-Id: Ibf6321b27cef97cc1144ce3202de0409f1620c6f

--- queue.c
+++ queue.c
@@ -19,9 +19,11 @@ void q_free(struct list_head *head)
 {
     if (!head)
         return;
-    struct list_head *node, *safe;
-    list_for_each_safe (node, safe, head)
+    element_t *node, *safe = NULL;
+    list_for_each_entry_safe (node, safe, head, list) {
+        free(node->value);
         free(node);
+    }
     free(head);
     return;
 }
